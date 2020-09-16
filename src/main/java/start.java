980dc9bb9c5cbd622a3412c4c24626961be0151e
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-09-16 15:12:12 -0300

    classes

--- src/main/java/start.java
+++ src/main/java/start.java
@@ -1,10 +1,29 @@
+import jade.core.Agent;
+
 public class start {
     public static void main(String[] args) {
+
+        // Inicia a G.U.I. para o JADE
         String[] parametros = {
                 "-gui",
                 "-local-host",
                 "127.0.0.1"
         };
         jade.Boot.main(parametros);
+
+        // Iniciando o container
+        String[] novoContainer = {
+                "-local-host", "127.0.0.1",
+                "-container", "-container-name",
+                "Container-Almeida"};
+        jade.Boot.main(novoContainer);
     }
+
+    public class AgenteComprador extends Agent {
+    }
+
+    public class AgenteVendedor extends Agent {
+    }
+
+
 }
